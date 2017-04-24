533cc0bf764caf82c096a9449af97333a7d6b95a
Joel Stuart <s4320371@labs.eait.uq.edu.au>
2017-04-24 17:26:15 +1000

    Added home and location pages

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SiK</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <div class="logo"><img src="img/SiKd.png" alt="Logo" width="70px" height="32px"></div>
+        <h6>Find Your Friends At Anytime</h6>
+        
+		<div class="mainArea">
+			<div class="leftHalf"> </div>
+			<div class="rightHalf"> </div>
+		</div>
+
+        <div id="footer">
+            Designed By Team.
+        </div>
+    </div>
+</body>
+</html>
---
+++ location.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SiK</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <div class="logo"><img src="img/SiKd.png" alt="Logo" width="70px" height="32px"></div>
+        <h6>Find Your Friends At Anytime</h6>
+        
+		<div class="mainArea">
+			<div class="map"> </div>
+		</div>
+
+        <div id="footer">
+            Designed By Team.
+        </div>
+    </div>
+</body>
+</html>
