2a019ae3bad177c3c14bb8f2ec9ef048d3cbfc5e
Joel Stuart <s4320371@labs.eait.uq.edu.au>
2017-05-12 15:11:29 +1000

    Basic task.php with links

---
+++ task.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SiK</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <div class="logo"><img src="img/SiKd.png" alt="Logo" width="70px" height="32px"></div>
+        <h6>Find Your Friends At Anytime</h6>
+        
+		<div class="mainArea">
+			<div style="width: 100%;">
+				<div style="float:left;">
+					<img src="img/blue.png"></img>
+					<h2><a href="home.php"> Find Friends</a></h2>
+				</div>
+				<div style="float:right;">
+					<img src="img/red.png"></img>
+					<h2><a href="location.php"> Go to Map</a></h2>
+				</div>
+			</div>
+			<div style="clear:both"></div>
+		</div>
+
+        <div id="footer">
+            Designed By Team.
+        </div>
+    </div>
+</body>
+</html>
