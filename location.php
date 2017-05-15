f8a11d1dd21c2a99252cc27770c726de1128f798
Joel Stuart <s4320371@labs.eait.uq.edu.au>
2017-05-15 16:50:45 +1000

    Google map test 2

--- location.php
+++ location.php
@@ -14,21 +14,14 @@ AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0
 
 <div id="googleMap" style="width:100%;height:400px;"></div>
 
-<script>
-function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-</script>
-
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0&callback=myMap"></script>
 
 
 <script>
     var x = document.getElementById("demo");
+	var lat = 0;
+	var longi = 0;
+	var map;
 
     function getLocation() {
         if (navigator.geolocation) {
@@ -41,7 +34,28 @@ var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
     function showPosition(position) {
         x.innerHTML = "Latitude: " + position.coords.latitude +
             "<br>Longitude: " + position.coords.longitude;
+		lat = position.coords.latitude;
+		longi = position.coords.longitude;
+		mapUpdate();
     }
+	
+	function myMap() {
+		var mapProp= {
+			center:new google.maps.LatLng(lat,longi),
+			zoom:5,
+		};
+		map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	}
+	
+	function mapUpdate() {
+		var mapProp= {
+			center:new google.maps.LatLng(lat,longi),
+			zoom:5,
+		};
+		map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	}
+	
+	//https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
 </script>
 
 </body>
