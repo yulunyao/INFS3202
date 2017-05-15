c9f8f0f9cf42f6d4610c470bd527d2f0c96b6add
Joel Stuart <s4320371@labs.eait.uq.edu.au>
2017-05-15 17:03:40 +1000

    Google lat longi test

--- location.php
+++ location.php
@@ -3,6 +3,7 @@
 <body>
 
 <!--
+Or key is:
 AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0
 -->
 
@@ -14,7 +15,7 @@ AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0
 
 <div id="googleMap" style="width:100%;height:400px;"></div>
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0&callback=myMap"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0&callback=initMap"></script>
 
 
 <script>
@@ -22,6 +23,8 @@ AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0
 	var lat = 0;
 	var longi = 0;
 	var map;
+	var geocoder;
+	var infowindow;
 
     function getLocation() {
         if (navigator.geolocation) {
@@ -36,26 +39,53 @@ AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0
             "<br>Longitude: " + position.coords.longitude;
 		lat = position.coords.latitude;
 		longi = position.coords.longitude;
+		console.log(lat);
+		console.log(longi);
 		mapUpdate();
     }
 	
-	function myMap() {
+	function initMap() {
 		var mapProp= {
 			center:new google.maps.LatLng(lat,longi),
-			zoom:5,
+			zoom:15,
 		};
 		map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+		geocoder = new google.maps.Geocoder;
+		infowindow = new google.maps.InfoWindow;
 	}
 	
 	function mapUpdate() {
 		var mapProp= {
 			center:new google.maps.LatLng(lat,longi),
-			zoom:5,
+			zoom:15,
 		};
 		map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 	}
 	
-	//https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+	
+	function geoLookup(){
+		var latlng = {lat: parseFloat(lat), lng: parseFloat(longi)};
+		geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[1]) {
+              map.setZoom(11);
+              var marker = new google.maps.Marker({
+                position: latlng,
+                map: map
+              });
+              infowindow.setContent(results[1].formatted_address);
+              infowindow.open(map, marker);
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+		
+	}
+	
+	//var json = https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0
 </script>
 
 </body>
