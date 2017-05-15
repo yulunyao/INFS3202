c955853ae407a3986c984b002a5462f010a1b3b0
Joel Stuart <s4320371@labs.eait.uq.edu.au>
2017-05-15 16:32:29 +1000

    Google map test

--- location.php
+++ location.php
@@ -12,6 +12,21 @@ AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0
 
 <p id="demo"></p>
 
+<div id="googleMap" style="width:100%;height:400px;"></div>
+
+<script>
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0&callback=myMap"></script>
+
+
 <script>
     var x = document.getElementById("demo");
 
