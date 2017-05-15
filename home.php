3ce0a0d86d17a274a98fb2486ca5c968bf3ae2bc
Joel Stuart <s4320371@labs.eait.uq.edu.au>
2017-05-15 18:12:12 +1000

    styling on post login pages

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>SiK</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/styleTask.css">
 </head>
 
 <body>
