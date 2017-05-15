ae0af669fe1cea8957d1bb0b8846bc45f24682dc
yulunyao <163alliance@gmail.com>
2017-05-15 22:14:03 +1000

    home.php

--- home.php
+++ home.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>SiK</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="../../Documents/GitHub/INFS3202/css/style.css">
 </head>
 
 <body>
     <div id="wrapper">
-        <div class="logo"><img src="img/SiKd.png" alt="Logo" width="70px" height="32px"></div>
+        <div class="logo"><img src="../../Documents/GitHub/INFS3202/img/SiKd.png" alt="Logo" width="70px" height="32px"></div>
         <h6>Find Your Friends At Anytime</h6>
         
 		<div class="mainArea">
