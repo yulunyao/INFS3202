a6e81655f1508dcba803ab47bbaf3163a3c7c968
yulunyao <163alliance@gmail.com>
2017-05-23 02:29:39 +1000

    add 'send_uid' page which allows user to type in uid in a form.

---
+++ send_uid.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+$con = mysqli_connect('au-cdbr-azure-east-a.cloudapp.net:3306', "b622a8e03ec7ba", "6e32c3d6", "sik");
+echo "Welcome " . $_SESSION['username'];
+$query = "SELECT random FROM signup WHERE username = '".$_SESSION['username']."'";
+$result = mysqli_query($con,$query);
+
+while($row = mysqli_fetch_assoc($result)) {
+    print_r(" with UID: ");
+    print_r($row["random"]);
+}
+echo "<a href='logout.php'> [logout]</a>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SiK</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+<div class="logo"><img src="img/SiKd.png" alt="Logo" width="70px" height="32px"></div>
+<h6>Find Your Friends At Anytime</h6>
+<div id="uid_img">
+    <img src="img/red.PNG" alt="Plane Image">
+</div>
+
+<div id="uid_container">
+    <h2>ENTER UID:</h2>
+    <form method="$_POST" action="getlocation.php">
+        <input required="required" type="number" name="uid">
+        <p></p>
+        <input type="submit" value="Go And Find">
+    </form>
+</div>
+
+
+    <div id="footer">
+        Designed By Team.
+    </div>
+
+</body>
+</html>
