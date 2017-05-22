b71e0892962a443185171ec3d0711a53026d0d61
JoelStuart <joel.stuart@uqconnect.edu.au>
2017-05-22 18:23:59 +1000

    Hash Fix 2

--- login.php
+++ login.php
@@ -22,8 +22,7 @@ if(isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
     }
     $sql = "SELECT * FROM signup WHERE (username = '$username')";
     $result = $conn->query($sql);
-	$hash = mysql_result($result, 0);
-    if ($result -> num_rows > 0 && hash_equals($hash, crypt($password, $hash))) {
+    if ($result -> num_rows > 0 && hash_equals($result, crypt($password, $result))) {
         if(isset($_REQUEST['rem'])) {
             setcookie('username', $username, time()+60*60*7);
             setcookie('password', $password, time()+60*60*7);
