a0807d938375b952d4c20d8c6096e5f9232400a4
JoelStuart <joel.stuart@uqconnect.edu.au>
2017-05-24 23:39:48 +1000

    Accessibility

--- location.php
+++ location.php
@@ -36,7 +36,7 @@ AIzaSyDG4jMSOZattisRWE3f96RaJcV5S9nQHr0
     <form method="$_POST" action="location_sent.php">
         <div id=right style="width: 50%; height: 100%; float:right ">
             <h3>You are now in the location:</h2>
-            <img src="img/blue.png"  width="50%" height="50%"></img>
+            <img src="img/blue.png" width="50%" height="50%" alt=""></img>
             <h2 id="addressA" name="address_detail"></h2>
             <input type="submit" value="Send Location" onclick="sendLocation()">
         </div>
