f436fbb1fad7616cfc039e01aaeb3454f7cd8161
JoelStuart <joel.stuart@uqconnect.edu.au>
2017-05-25 13:07:01 +1000

    Missed include in send uid fix

--- uid_check.php
+++ uid_check.php
@@ -5,8 +5,11 @@
  * Date: 2017/5/23
  * Time: 20:55
  */
-$con = mysqli_connect('au-cdbr-azure-east-a.cloudapp.net:3306', "b622a8e03ec7ba", "6e32c3d6", "sik");
-
+ 
+include('connectMySQL.php'); //make sure the path is correct.
+//$con = mysqli_connect('au-cdbr-azure-east-a.cloudapp.net:3306', "b622a8e03ec7ba", "6e32c3d6", "sik");
+$db = new MySQLDatabase(); //create a Database object
+$con = $db->connect("root", "", "sik");
 $uid = $_REQUEST['uid'];
 
 session_start();
@@ -18,7 +21,7 @@ $_SESSION['uid'] = $uid;
 
 
 //Protected SQL
-$stmt = $con->link->prepare("SELECT random FROM uid WHERE random = ?");
+$stmt = $db->link->prepare("SELECT random FROM uid WHERE random = ?");
 $stmt->bind_param($result, "s", $uid);
 $stmt->execute($result);
 
@@ -46,4 +49,5 @@ if(stmt->num_rows($result ) > 0) {
 }
 
 $stmt->close();
+$db->disconnect();
 ?>
