7284edac07301adb23ecf284c94ada3c236a64a5
JoelStuart <joel.stuart@uqconnect.edu.au>
2017-05-25 13:23:39 +1000

    getLocation fix

--- getlocation.php
+++ getlocation.php
@@ -9,7 +9,7 @@ session_start();
 include("connectMySQL.php");
 $con = mysqli_connect('au-cdbr-azure-east-a.cloudapp.net:3306', "b622a8e03ec7ba", "6e32c3d6", "sik");
 echo "Welcome " . $_SESSION['username'];
-$query = "SELECT random FROM signup WHERE username = '".$_SESSION['username']."'";
+$query = "SELECT random FROM uid WHERE username = '".$_SESSION['username']."'";
 $result = mysqli_query($con,$query);
 
 /*Show Welcome information with current username and UID.*/
@@ -27,7 +27,7 @@ echo("<p></p>");
 
 /*Display the UID and the location correspond to that UID.*/
 
-$query = "SELECT location FROM signup WHERE random= $uid";
+$query = "SELECT location FROM uid WHERE random= $uid";
 $uid_location = mysqli_query($con, $query);
 
 while ($row = mysqli_fetch_assoc($uid_location)) {
