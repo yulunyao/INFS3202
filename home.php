e65b5e710856d90b74c2fe02ecda0da0e66abfc6
max96ellis <max96ellis@gmail.com>
2017-05-28 11:42:25 +1000

    Update home.php

--- home.php
+++ home.php
@@ -19,8 +19,8 @@
 	</section>
 	<p>
 	<br>
-		SiK is a simple and easy to use tool! <br>
-		Press ‘Send Location’ or ‘Find Friends’ button, the former one allows user to share their location to their friends, the latter one allows user to get the location of their friends based on their UID (User Identifier). The UID is a six-digit number to recognize users. If user typed in the UID, an embedded google map will show the location of the user correspond to that UID. If user needs to find that user, simply press ‘Go and Find’ button, then web page will be directed to Google Map to guide user to destination.
+		SiK is a simple and easy to use tool to help find your friends! <br>
+		Login then either press ‘Send Location’ or ‘Find Friends’. SiK uses individual geotracking to be able to share your location or find friends in the area. It can be used as a convenient way of keeping an eye on the kids, making sure everyone meets at the same restaurant, or simply if you want to check to see what friends or family are in the area. To get the location of your friends is based on their UID (User Identifier). The UID is a six-digit number to recognize users. If user typed in the UID, an embedded google map will show the location of the user correspond to that UID. If user needs to find that user, simply press ‘Go and Find’ button, then web page will be directed to Google Map to guide user to destination.
 		<br>
 		<br>
 	</p>
